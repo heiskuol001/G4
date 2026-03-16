3bc92f22da5641ac1cfc1287ff88cc6982bc78d3
Kuol Magok <heiskuol001@gmail.com>
2026-03-17 01:06:25 +0300

    add: frame settings

--- src/main/java/org/example/BorderLayoutFrame.java
+++ src/main/java/org/example/BorderLayoutFrame.java
@@ -8,5 +8,10 @@ public class BorderLayoutFrame extends JFrame {
 
     public BorderLayoutFrame() {
 
+        setTitle("BorderLayout Demo");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
     }
 }
